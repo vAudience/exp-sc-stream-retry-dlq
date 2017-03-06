30411c6d4147800fa1577f230cf13abc5e14d14f
michele <michele@cheergg.com>
2017-03-06 14:21:44 +0000

    concurrency test

--- src/main/kotlin/com/example/Consumer.kt
+++ src/main/kotlin/com/example/Consumer.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
@@ -11,6 +12,7 @@ import org.springframework.messaging.SubscribableChannel
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.stereotype.Service
+import java.text.SimpleDateFormat
 
 
 @EnableScheduling
@@ -26,12 +28,23 @@ fun main(args: Array<String>) {
 @EnableBinding(Processor::class)
 class MyConsumer {
 
+    private val log = LoggerFactory.getLogger(MyConsumer::class.java)
+
     @SendTo(Processor.OUTPUT)
     @StreamListener(Processor.INPUT)
     fun handle(data: String): String {
-        println("received: $data")
-        throw RuntimeException()
-//        return data
+        val sdfDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val date = sdfDate.parse(data)
+        val seconds = date.seconds
+
+        log.info("received: $data --> $seconds")
+
+        if (seconds % 2 == 0) {
+            log.warn("throwing error...")
+            throw RuntimeException()
+        }
+
+        return data
     }
 
 }
